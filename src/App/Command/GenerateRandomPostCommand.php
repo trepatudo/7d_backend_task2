54e302f24db3398bf7b1558409ec8d73262fe172
Marco Silva <dev@marcosilva.pt>
2024-04-30 01:51:30 +0100

    feat(GenerateRandomPostCommand): add error handler and use app date format

--- src/App/Command/GenerateRandomPostCommand.php
+++ src/App/Command/GenerateRandomPostCommand.php
@@ -4,33 +4,41 @@ namespace App\Command;
 
 use App\Entity\Post;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use joshtronic\LoremIpsum;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 class GenerateRandomPostCommand extends Command
 {
-    protected static $defaultName = 'app:generate-random-post';
+    protected static $defaultName        = 'app:generate-random-post';
     protected static $defaultDescription = 'Generate a random post';
 
     private EntityManagerInterface $em;
-    private LoremIpsum $loremIpsum;
+    private LoremIpsum             $loremIpsum;
 
-    public function __construct(EntityManagerInterface $em, LoremIpsum $loremIpsum, string $name = null)
-    {
-        parent::__construct($name);
-        $this->em = $em;
+    private string $dateFormat;
+
+    public function __construct(
+        EntityManagerInterface $em,
+        LoremIpsum $loremIpsum,
+        string $name = null,
+        string $dateFormat
+    ) {
+        $this->em         = $em;
         $this->loremIpsum = $loremIpsum;
+        $this->dateFormat = $dateFormat;
+
+        parent::__construct($name);
     }
 
     protected function configure(): void
     {
         $this
-            ->addOption('summary', 's', InputOption::VALUE_NONE, 'Generate the summary of the day instead of fully random');
+            ->addOption('summary', 's', InputOption::VALUE_NONE,
+                'Generate the summary of the day instead of fully random');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -44,22 +52,30 @@ class GenerateRandomPostCommand extends Command
         $post->setTitle($title);
         $post->setContent($content);
 
-        $this->em->persist($post);
-        $this->em->flush();
+        try {
+            $this->em->persist($post);
+            $this->em->flush();
 
-        $output->writeln(sprintf('A %s post has been generated.', $isSummaryPost ? 'summary' : 'random'));
+            $output->writeln(sprintf('A %s post has been generated.', $isSummaryPost ? 'summary' : 'random'));
+        } catch (Exception $e) {
+            $output->writeln('<error>An error occurred while trying to generate post</error>');
+            return Command::FAILURE;
+        }
 
         return Command::SUCCESS;
     }
 
-    private function getRandomPostData(): array {
-        $title = $this->loremIpsum->words(mt_rand(4, 6));
+    private function getRandomPostData(): array
+    {
+        $title   = $this->loremIpsum->words(mt_rand(4, 6));
         $content = $this->loremIpsum->paragraphs(2);
         return [$title, $content];
     }
-    private function getSummaryData(): array {
+
+    private function getSummaryData(): array
+    {
         $title = "Summary ";
-        $title .= date("YYYY-MM-DD");
+        $title .= date($this->dateFormat);
 
         $content = $this->loremIpsum->paragraphs(1);
         return [$title, $content];
